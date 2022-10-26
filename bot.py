0965f4cb3da1fae5190361ed3b675bb8e9e5ab0f
edgarsvolfs <99868766+edgarsvolfs@users.noreply.github.com>
2022-10-26 21:44:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import random
 
 
-TOKEN = 'ODk4OTAwMTg3OTY2MjE4MjUy.G8gdc4.ziqH4pKQ7Ki7Lq7q9zO7PNH2-4b2AkWRKnDWlA'
+TOKEN = ''  # insert token
 
 intents = discord.Intents.all()
 intents.members = True
@@ -13,7 +13,7 @@ client = discord.Client(command_prefix="$", intents=intents)
 @client.event
 async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name='Plebs')
-    channel = client.get_channel(898900471618621453)  # channela id
+    channel = client.get_channel(401071542110388257)  # channela id
     await member.add_roles(role)
     await channel.send(f'> Hey {member.mention}, welcome to **HeHeXD**')
 
@@ -22,7 +22,7 @@ async def on_member_join(member):
 async def on_member_remove(member):
     member_name = str(member).split('#')[0]
     print('left')
-    channel = client.get_channel(898900471618621453)  # channela id
+    channel = client.get_channel(401071542110388257)  # channela id
     await channel.send(f'> **{member_name}** finally pissed off')
 
 
@@ -33,62 +33,14 @@ async def on_message(message):
     content = str(message.content)
 
     print(f'{username}: {content} ({channel})')
-    #  print('message content ' + message.len)
+
     if message.author == client.user:
         return
 
-    # if author.id == 369539585342177286:
-    # s = random.randint(0, 100)
-    #  if s > 95:
-    #       await message.channel.send(f'{username}, stfu dog')
-    #        return
-
-    s = random.randint(0, 150)
-    if s > 149:
+    s = random.randint(0, 500)
+    if s > 499:
         await message.channel.send(f'yes but who asked?')
         return
-
-    # @client.event
-    # async def on_message(message):
-    #     if message.content.startswith('!cannon'):
-    #         channel = message.channel
-    #         txt = message.content
-    #         x = txt.split(" ")
-    #
-    #         if x[1] == 'add':
-    #             try:
-    #                 dict[x[2]] += 1
-    #             except:
-    #                 dict[x[2]] = 1
-    #         elif x[1] == 'top':
-    #             for key in dict:
-    #                 await channel.send(key, bot.get_user(dict[2]))
-    #
-    #         # vajag noformet dict, var vienk printet ara
-    #
-    #         await channel.send(dict)
-
-    # if ".guess" in user_message:
-    #     channel = message.channel
-    #     await message.channel.send(f'guess a number 1-1000')
-    #     rez = random.randint(0, 1000)
-    #     while (x != rez):
-    #         if x > rez:
-    #             await message.channel.send(f'go lower')
-    #             x = int(user_message)
-    #         elif x < rez:
-    #             await message.channel.send(f'go higher')
-    #             x = int(user_message)
-    #     await message.channel.send(f'OOOOKAY')
-    #
-    # if message.channel.name == 'general':
-    # if user_message.lower() == 'hello':
-    #    await message.channel.send(f'Hello {username}!')
-    #    return
-    # elif user_message.lower() == 'bye':
-    #    await message.channel.send(f'cy@ {username}!')
-    #    return
-
 
 @client.event
 async def on_ready():
